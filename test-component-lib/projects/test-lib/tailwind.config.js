d7d97504ffb1f9e162850cd952026f81809ec413
Huu Tung Nguyen <ga87cer@mytum.de>
2023-03-13 22:56:21 +0100

    added tailwindcss

---
+++ test-component-lib/projects/test-lib/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./projects/test-lib/**/*.{html,ts,css,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
